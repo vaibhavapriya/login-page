96014f0c2d1e9ad19c63ef8de2c7880e33dfa030
vaibhavapriya <vaibhavapriyandv@gmail.com>
2024-12-17 19:14:49 +0530

    frontend

---
+++ front-end/vite-project/src/App.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import ForgotPassword from './components/ForgotPassword';
+import ResetPassword from './components/ResetPassword';
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<ForgotPassword />} />
+        <Route path="/reset-password/:token" element={<ResetPassword />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
