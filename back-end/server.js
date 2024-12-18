561dfcf5cef2c46615634d93d355936d531651c1
vaibhavapriya <vaibhavapriyandv@gmail.com>
2024-12-18 17:23:07 +0530

    server

--- back-end/server.js
+++ back-end/server.js
@@ -1,26 +1,26 @@
 const express = require('express');
+const cors = require('cors');
 const connectDB = require('./config/db');
 const authRoutes = require('./routes/loginRoutes');
 const errorHandler = require('./middleware/errorHandler');
-//const errorHandler = require('./middleware/validToken');
 require('dotenv').config();
-const cors = require('cors');
-app.use(cors());
 
-
+// Initialize the app
 const app = express();
 
 // Connect to MongoDB
 connectDB();
 
 // Middleware
+app.use(cors()); // Use CORS middleware
 app.use(express.json());
 
 // Routes
 app.use('/api', authRoutes);
 
+// Error Handling Middleware
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
